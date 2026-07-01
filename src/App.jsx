d625925cee861fc6ae1af9d16a7c62315e486e44
manojit17 <manojit75431@gmail.com>
2026-07-01 23:40:55 +0530

    small changes is app.jsx1

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import ReportIssue from './pages/ReportIssue'
 import IssueMap from './pages/IssueMap'
 import Profile from './pages/Profile'
 import Community from './pages/Community'
-import VerifyIssues from './pages/VerifyIssues'
+import Verify from './pages/VerifyIssues'
 
 export default function App() {
   const [page, setPage] = useState('landing')
@@ -28,7 +28,8 @@ export default function App() {
      case 'profile'  : return <Profile   navigate={navigate} />   
      default         : return <Landing   navigate={navigate} />
      case 'community': return <Community navigate={navigate} />
-     case 'Verify': return <VerifyIssues navigate={navigate}/>
+     case 'verify'   : return <VerifyIssues navigate={navigate} />
+    default         : return <Landing   navigate={navigate} />
    }
  }
 
